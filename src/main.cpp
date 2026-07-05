2b64f90aca9aa8dc1b5c8945590aebed3fea6c50
darrenh124 <darrenhandjojo124@gmail.com>
2026-07-05 22:51:31 +0000

    Initial Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <iomanip>
 #include <iostream>
 #include "receipt.hpp"
-
 using namespace std;
 
 int main() {
-    double item1 = 0.0;
-    double item2 = 0.0;
-    double item3 = 0.0;
-    double tipPercent = 0.0;
+    double item1, item2, item3;
+ 
+    double tipPercent;
+
+
 
     cout << "Receipt Calculator" << endl << endl;
 
@@ -24,17 +24,22 @@ int main() {
     cout << "Enter tip percentage: ";
     cin >> tipPercent;
 
+
+
     double subtotal = calculateSubtotal(item1, item2, item3);
     double tax = calculateTax(subtotal);
     double tip = calculateTip(subtotal, tipPercent);
     double total = calculateTotal(subtotal, tax, tip);
 
+
+    
     cout << fixed << setprecision(2);
     cout << endl;
     cout << "Subtotal: $" << subtotal << endl;
     cout << "Tax: $" << tax << endl;
     cout << "Tip: $" << tip << endl;
     cout << "Total: $" << total << endl;
+
 
     return 0;
 }
